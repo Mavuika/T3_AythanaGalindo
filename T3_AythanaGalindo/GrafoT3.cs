ab75728253508cfb8ec9cf9e1a064732aa3790a7
UPN <UPN@ULCLAB10XX>
2025-06-19 16:29:04 -0500

    Agregar archivos de proyecto.

---
+++ T3_AythanaGalindo/GrafoT3.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3_AythanaGalindo
+{
+    internal class GrafoT3
+    {
+        private double[,] matrizAdyacencia;
+        private int numNodos;
+        public GrafoT3(int numNodos)
+        {
+            this.numNodos = numNodos;
+            matrizAdyacencia = new double[numNodos, numNodos];
+
+            for (int i = 0; i < numNodos; i++)
+            {
+                for (int j = 0; j < numNodos; j++)
+                {
+                    if (i == j)
+                        matrizAdyacencia[i, j] = 0;
+                    else
+                        matrizAdyacencia[i, j] = double.PositiveInfinity;
+                }
+            }
+        }
+
+        public void AgregarArista(int origen, int destino, double peso)
+        {
+            matrizAdyacencia[origen, destino] = peso;
+            matrizAdyacencia[destino, origen] = peso;
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Matriz de adyacencia:");
+            for (int i = 0; i < numNodos; i++)
+            {
+                for (int j = 0; j < numNodos; j++)
+                {
+                    if (double.IsPositiveInfinity(matrizAdyacencia[i, j]))
+                        Console.Write(" ∞ ");
+                    else
+                        Console.Write($"{matrizAdyacencia[i, j],3} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public int NumGrupos()
+        {
+            bool[] visitado = new bool[numNodos];
+            int grupos = 0;
+            for (int i = 0; i < numNodos; i++)
+            {
+                if (!visitado[i])
+                {
+                    grupos++;
+                    DFS(i, visitado);
+                }
+            }
+            return grupos;
+        }
+
+        public bool GrupoCorrelativo(int nodo)
+        {
+            List<int> grupo = new List<int>();
+            bool[] visitado = new bool[numNodos];
+
+            DFSCollect(nodo, visitado, grupo);
+
+            grupo.Sort();
+
+            for (int i = 1; i < grupo.Count; i++)
+            {
+                if (grupo[i] != grupo[i - 1] + 1)
+                    return false;
+            }
+            return true;
+        }
+
+        private void DFS(int nodo, bool[] visitado)
+        {
+            visitado[nodo] = true;
+
+            for (int i = 0; i < numNodos; i++)
+            {
+                if (matrizAdyacencia[nodo, i] != double.PositiveInfinity && matrizAdyacencia[nodo, i] != 0 && !visitado[i])
+                {
+                    DFS(i, visitado);
+                }
+            }
+        }
+
+        private void DFSCollect(int nodo, bool[] visitado, List<int> grupo)
+        {
+            visitado[nodo] = true;
+            grupo.Add(nodo);
+
+            for (int i = 0; i < numNodos; i++)
+            {
+                if (matrizAdyacencia[nodo, i] != double.PositiveInfinity && matrizAdyacencia[nodo, i] != 0 && !visitado[i])
+                {
+                    DFSCollect(i, visitado, grupo);
+                }
+            }
+
+        }
+
+    }
+}
+
+
+
---
+++ T3_AythanaGalindo/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3_AythanaGalindo
+{
+    class Program
+    {
+        static GrafoT3 grafo = null;
+        static void Main(string[] args)
+        {
+            int opcion;
+            do
+            {
+                Console.WriteLine("\n   m e n u    ");
+                Console.WriteLine("1.  Agregar arista");
+                Console.WriteLine("2.  Mostrar matriz");
+                Console.WriteLine("3.  Cantidad de grupos");
+                Console.WriteLine("4.  Ccorrelativo");
+                Console.WriteLine("5.  Salir");
+
+                Console.Write("Seleccione una opcion: ");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        AgregarArista();
+                        break;
+
+                    case 2:
+                        MostrarMatriz();
+                        break;
+
+                    case 3:
+                        MostrarCantidadGrupos();
+                        break;
+
+                    case 4:
+                        EsCorrelativo();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Cerrando");
+                        break;
+
+                    default:
+                        Console.WriteLine("Ingrese una opcion nvalida.");
+                        break;
+
+                }
+
+
+
+            } while (opcion != 5);
+
+        }
+
+        static void AgregarArista()
+        {
+
+            if (grafo == null)
+            {
+                Console.Write("Ingrese cantidad de nodos: ");
+                int cantidad = int.Parse(Console.ReadLine());
+                grafo = new GrafoT3(cantidad);
+            }
+
+            Console.Write("Ingrese nodo origen: ");
+            int origen = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese nodo destino: ");
+            int destino = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese peso: ");
+            double peso = double.Parse(Console.ReadLine());
+            grafo.AgregarArista(origen, destino, peso);
+            Console.WriteLine("Arista agregada.");
+        }
+
+        static void MostrarMatriz()
+        {
+            if (grafo == null)
+            {
+                Console.WriteLine("El grafo no ha sido creado.");
+                return;
+            }
+
+            grafo.Mostrar();
+        }
+
+        static void MostrarCantidadGrupos()
+        {
+
+            if (grafo == null)
+            {
+                Console.WriteLine("El grafo no ha sido creado.");
+
+                return;
+            }
+            int grupos = grafo.NumGrupos();
+            Console.WriteLine($"Cantidad de grupos: {grupos}");
+        }
+
+        static void EsCorrelativo()
+        {
+            if (grafo == null)
+            {
+                Console.WriteLine("El grafo no ha sido creado.");
+                return;
+            }
+
+            Console.Write("Ingrese nodo: ");
+            int nodo = int.Parse(Console.ReadLine());
+            bool esCorrelativo = grafo.GrupoCorrelativo(nodo);
+            Console.WriteLine($"El grupo es correlativo: {esCorrelativo}");
+
+        }
+
+    }
+}
+
+
+
